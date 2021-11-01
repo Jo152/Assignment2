5db2f3afd0fdf5629d7f5262ecf145682d94b90f
Jonathan Euzebe <60105810+Jo152@users.noreply.github.com>
2021-10-31 22:50:38 -0400

    fixes

--- app/views/Default/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-	<title>Starting location</title>
-</head>
-<body>
-	<h1>Hello, please login if you want to proceed</h1>
-	<p><a href='<?= BASE ?>/Default/login'>Login</a></p>
-</body>
-</html>
